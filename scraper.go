6450e9a5962868c66418fca9d23eb9d3d73e2ed6
Vladislav Iurev <vlad.yurev.06@gmail.com>
2025-06-25 18:29:38 +0400

    implemented multy-paeg scraping and formating to a JSON

--- scraper.go
+++ scraper.go
@@ -1,25 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
+type item struct {
+	Name    string `json:"name"`
+	Article string `json:"article"`
+	Price   string `json:"price"`
+	ImgUrl  string `json:"imgurl"`
+}
+
 func main() {
 
 	c := colly.NewCollector(
-		colly.AllowedDomains("skyscanner.net"),
+		colly.AllowedDomains("elitesport.ge"),
 	)
-	c.OnHTML("div[class=EcoTicketWrapper_ecoContainer_*]", func(h *colly.HTMLElement) {
-		fmt.Println(h.Text)
+
+	var items []item
+
+	c.OnHTML("ul#catalog_holder div[class=catalog_list_info]", func(h *colly.HTMLElement) {
+		item := item{
+			Name:    h.ChildText("a.catalog_list_name"),
+			Article: h.ChildText("span.catalog_list_article"),
+			Price:   h.ChildText("span.catalog_list_actual_price"),
+			ImgUrl:  h.ChildAttr(".catalog_list_img_wrap_item img", "src"),
+		}
+		// fmt.Printf("Item is : %v\n", items)
+		items = append(items, item)
+	})
+
+	c.OnHTML("ul.pagin_list a.next", func(h *colly.HTMLElement) {
+
+		next_page := h.Request.AbsoluteURL(h.Attr("href"))
+		fmt.Printf(next_page)
+		c.Visit(next_page)
 
 	})
-	c.Visit("https://www.skyscanner.net/transport/flights/kut/opo/250823/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false")
-}
 
-type item struct {
-	Name   string `json"name"`
-	Price  string `json"price"`
-	ImgUrl string `json"imgurl"`
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println(r.URL.String())
+	})
+
+	c.Visit("https://elitesport.ge/ru/mens/")
+	fmt.Println(items)
+	content, err := json.Marshal(items)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	os.WriteFile("products.json", content, 0644)
 }
