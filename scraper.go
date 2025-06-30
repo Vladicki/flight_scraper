76af77299d20f41cbda2248ae21901b59008a492
Vladislav Iurev <vlad.yurev.06@gmail.com>
2025-07-01 00:03:21 +0400

    changed comments?

--- scraper.go
+++ scraper.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	getFlights()
+
 }
 
 func getFlights() {
@@ -17,22 +18,33 @@ func getFlights() {
 			g.GetRendered("https://www.skyscanner.ie/transport/flights/rmo/bus/250822/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false", g.Opt.ParseFunc)
 		},
 		// StartURLs: []string{"https://www.skyscanner.ie/transport/flights/rmo/bus/250822/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false"},
-		ParseFunc:       skyParse,
-		Exporters:       []export.Exporter{&export.CSV{}},
-		BrowserEndpoint: "ws://localhost:9222",
+		// StartURLs: []string{"https://www.skyscanner.ie/transport/flights/rmo/bus/250822/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false"},
+		// StartURLs: []string{"https://www.skyscanner.ie/transport/flights/rmo/bus/250822/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false"},
+		ParseFunc: skyParse,
+		Exporters: []export.Exporter{&export.CSV{}},
+		// BrowserEndpoint: "ws://localhost:9222",
 	}).Start()
 }
 
 func skyParse(g *geziyor.Geziyor, r *client.Response) {
-	r.HTMLDoc.Find("div[class ^='FlightsTicket_container'").Each(func(i int, s *goquery.Selection) {
+	// Find each ticket container
+	r.HTMLDoc.Find("div[class^='FlightsTicket_container']").Each(func(i int, s *goquery.Selection) {
+
+		// Extract price from .Price_mainPriceContainer > span
+		price := s.Find("div.Price_mainPriceContainer span").First().Text()
+
+		// Extract leg info text from all inner divs inside .LegInfo_legInfo
+		legInfo := ""
+		s.Find("div.LegInfo_legInfo div").Each(func(i int, div *goquery.Selection) {
+			legInfo += div.Text() + " "
+		})
+
+		// Export data
 		g.Exports <- map[string]interface{}{
-			"text":   s.Find("div[class ^='Ticket'").Text(),
-			"author": s.Find("small.author").Text(),
+			"price":   price,
+			"legInfo": legInfo,
 		}
 	})
-	if href, ok := r.HTMLDoc.Find("li.next > a").Attr("href"); ok {
-		g.Get(r.JoinURL(href), skyParse)
-	}
 
 	//	func quotesParse(g *geziyor.Geziyor, r *client.Response) {
 	//		r.HTMLDoc.Find("div.quote").Each(func(i int, s *goquery.Selection) {
